7cc18173f479f1901cb9fc5af54ac2bea5b67679
Vinicius Fioravante <vs.fioravante@gmail.com>
2019-03-27 23:53:00 -0300

    Add de loop para percorrer string de palavra secreta para a execução do jogo de forca

--- forca.py
+++ forca.py
@@ -3,6 +3,20 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+
+        chute = input("Qual letra?")
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute == letra):
+                print("Encontrei a letra '{}' na posição {} ".format(letra, index) )
+            index = index + 1
+
     print("Fim do jogo")
 
 if(__name__ == "__main__"):
