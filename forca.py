ecdc33479c1e75dd1d5e886e0e87989273d6db7d
Vinicius Fioravante <vs.fioravante@gmail.com>
2019-03-28 22:34:45 -0300

    Add de lista para armazenar as letras corretas inseridas pelo usuario

--- forca.py
+++ forca.py
@@ -4,8 +4,11 @@ def jogar():
     print("*********************************")
 
     palavra_secreta = "banana"
+    letras_acertadas = ["_","_","_","_","_","_"]
     enforcou = False
     acertou = False
+
+    print(letras_acertadas)
 
     while(not enforcou and not acertou):
 
@@ -15,8 +18,10 @@ def jogar():
         index = 0
         for letra in palavra_secreta:
             if(chute.upper() == letra.upper()):
-                print("Encontrei a letra '{}' na posição {} ".format(letra, index) )
+                letras_acertadas[index] = letra
+
             index = index + 1
+        print(letras_acertadas)
 
     print("Fim do jogo")
 
